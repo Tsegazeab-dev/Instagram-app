613433e2816e5e176a22253d14ef5990beba414e
Tsegazeab <tsegazeabwuta@gmail.com>
2023-10-19 04:06:01 -0700

    Add search bar to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import {AiOutlineSearch} from "react-icons/ai"
 
 export default function Header(){
     return (
-        <div className="flex items-center justify-between max-w-6xl mx-auto">
+        <div className="flex items-center justify-between max-w-6xl mx-auto mt-3">
             {/* left side */}
             <div className="cursor-pointer hidden lg:inline-grid">
                 <Image
@@ -18,6 +19,11 @@ export default function Header(){
                 width= {40}
                 height={100}
                 />
+            </div>
+            {/* Middle */}
+            <div className="relative">
+                <AiOutlineSearch className="absolute top-3 left-2 text-gray-500"/>
+                <input type="text" placeholder="search" className="border-gray-500 text-sm rounded-md h-9 pl-8 bg-gray-50 focus:ring-black focus:border-black" />
             </div>
             {/* right side */}
             <h1>Right side</h1>
