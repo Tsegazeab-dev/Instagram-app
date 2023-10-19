55a184086526a8e23a8061bdb1fd51fe5ea571c0
Tsegazeab <tsegazeabwuta@gmail.com>
2023-10-19 05:05:04 -0700

    Add menu and profile to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import {AiOutlineSearch} from "react-icons/ai"
+import {AiOutlineSearch, AiFillHome, AiOutlinePlusCircle} from "react-icons/ai"
 
 export default function Header(){
     return (
-        <div className="flex items-center justify-between max-w-6xl mx-auto mt-3">
+        <div className="flex items-center justify-between mx-4 max-w-6xl xl:mx-auto mt-5">
             {/* left side */}
             <div className="cursor-pointer hidden lg:inline-grid">
                 <Image
@@ -20,13 +20,19 @@ export default function Header(){
                 height={100}
                 />
             </div>
+            
             {/* Middle */}
             <div className="relative">
                 <AiOutlineSearch className="absolute top-3 left-2 text-gray-500"/>
                 <input type="text" placeholder="search" className="border-gray-500 text-sm rounded-md h-9 pl-8 bg-gray-50 focus:ring-black focus:border-black" />
             </div>
             {/* right side */}
-            <h1>Right side</h1>
+            <div className="flex items-center space-x-4">
+                <AiFillHome className="hidden md:inline-flex text-2xl cursor-pointer"/>
+                <AiOutlinePlusCircle className="text-2xl cursor-pointer"/>
+
+                <img src="https://media.licdn.com/dms/image/D4E03AQHBjOrw1Q6Wkg/profile-displayphoto-shrink_800_800/0/1674905933454?e=2147483647&v=beta&t=703Vp4meIvx6sflUjurVVtvPJpxXCQj9zBjxBz7Jx_w" alt="profile" className="h-12 cursor-pointer rounded-full" />
+            </div>
         </div>
     )
 }
