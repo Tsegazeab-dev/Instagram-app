615edaba3cc618f3b2370f77b5ae9233ddcf1b21
Tsegazeab <tsegazeabwuta@gmail.com>
2023-10-20 20:24:54 -0700

    Add the post component header

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,16 @@
-export default function Post({username, img, caption, profileImg, id}) {
+import {BsThreeDots} from 'react-icons/bs'
+export default function Post({username, img, caption, userImg, id}) {
   return (
-    <div>
-        <h1>{username}</h1>
-        <img src={img} alt={caption} />
+    <div className=' bg-white my-7  border rounded-md'>
+        {/* posts Header */}
+        <div className="flex items-center p-5">
+            <img className='w-12 rounded-full mr-3 p-1'src={userImg} alt={username} />
+            <p className='font-bold flex-1'>{username}</p>
+            <BsThreeDots/>
+        </div>
+        {/* post images */}
+        
+        <img className='object-cover w-full'src={img} alt={caption} />
         <p>{caption}</p>
     </div>
   )
