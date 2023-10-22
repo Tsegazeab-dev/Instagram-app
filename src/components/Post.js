212f2a4b8d8eb2f55dd6aa429c29f2728a2209a1
Tsegazeab <tsegazeabwuta@gmail.com>
2023-10-22 10:09:19 -0700

    Add post comment and comment box

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,6 @@
 import {BsThreeDots, BsChatDots, BsBookmark} from 'react-icons/bs'
 import {AiOutlineHeart} from 'react-icons/ai'
+import {BiHappy} from "react-icons/bi"
 export default function Post({username, img, caption, userImg, id}) {
   return (
     <div className=' bg-white my-7  border rounded-md'>
@@ -12,7 +13,6 @@ export default function Post({username, img, caption, userImg, id}) {
         {/* post images */}
         
         <img className='object-cover w-full'src={img} alt={caption} />
-        {/* <p>{caption}</p> */}
 
         {/* post buttons */}
         <div className="flex justify-between px-4 pt-4">
@@ -22,6 +22,16 @@ export default function Post({username, img, caption, userImg, id}) {
           </div>
           <BsBookmark className='btn'/>
         </div>
+
+        {/* post comments */}
+        <p className="p-5 truncate"><span className='font-bold mr-2'>{username}</span>{caption}</p>
+
+        {/* post comment box */}
+        <form className="flex items-center p-4">
+          <BiHappy className='text-xl'/>
+          <input type="text" placeholder='Enter your comment...' className='flex-1 border-none focus:ring-0' />
+          <button className='font-bold text-blue-400'>post</button>
+        </form>
     </div>
   )
 }
