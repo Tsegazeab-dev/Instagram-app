d5f7bdd945ea3804ad0bb39f52e80c4e343bcd3b
Tsegazeab <tsegazeabwuta@gmail.com>
2023-10-22 10:24:44 -0700

    make the feed page responsive

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,14 +3,14 @@ import Stories from "./Stories";
 
 function Feed() {
   return (
-    <main>
-   <section>
+    <main className="grid md:grid-cols-3 max-w-6xl mx-auto">
+   <section className="md:col-span-2">
     {/* stories */}
     <Stories/>
     {/* posts */}
     <Posts/>
    </section>
-   <section>
+   <section className="hidden md:inline-grid col-span-1">
     {/* profile */}
     {/* sugesition */}
    </section>
