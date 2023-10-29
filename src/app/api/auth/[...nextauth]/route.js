37f1e927325225a1cca8473691a65b9cd09ec500
Tsegazeab <tsegazeabwuta@gmail.com>
2023-10-29 07:14:16 -0700

    set up firebase & next auth for authentication

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+export default async function auth(req, res) {
+  const providers = [
+    GoogleProvider(
+      {
+        clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }
+    ),
+  ]
+
+  const isDefaultSigninPage = req.method === "GET" && req.query.nextauth.includes("signin")
+
+  // Will hide the `GoogleProvider` when you visit `/api/auth/signin`
+  if (isDefaultSigninPage) providers.pop()
+
+  return await NextAuth(req, res, {
+    providers,
+
+  })
+}
